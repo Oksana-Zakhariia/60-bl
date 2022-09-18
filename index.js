9fe9f16d3674f451f9275916117b3c885cc6df17
Oksana-Zakhariia <okcahka.l@gmail.com>
2022-09-18 14:56:05 +0300

    task-1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log(1)
+//Використовуючи функцію if...else,
+//напишіть код, який запитуватиме:
+//"Яка офіційна назва JavaScript?"
+//Якщо користувач вводить "ECMAScript",
+// показати через alert: "Вірно!"
+//інакше відобразити:"Не знаєте? ECMAScript!"
+const userInput = prompt("Яка офіційна назва JavaScript?")
+console.log(userInput);
+if (userInput === "ECMAScript") {
+    alert("Вірно!")
+    
+} else {
+    alert("Не знаєте? ECMAScript!")
+    
+}
